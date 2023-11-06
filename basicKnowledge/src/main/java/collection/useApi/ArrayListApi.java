ec31124b4a2b589a997e2af0a580bf0ec82df6d2
wuzhangchao <wuzhangchao@jiuqi.com.cn>
2023-11-06 17:28:26 +0800

    test

---
+++ basicKnowledge/src/main/java/collection/useApi/ArrayListApi.java
@@ -0,0 +1,16 @@
+package collection.useApi;
+
+/**
+ * @author wuzhangchao
+ * @version 1.0
+ * @date 2023/11/6 17:25
+ */
+
+/**
+ * 学习ArrayList
+ */
+public class ArrayListApi {
+    public void add(){
+
+    }
+}
---
+++ basicKnowledge/src/test/java/collection/useApi/ArrayListApiTest.java
@@ -0,0 +1,9 @@
+package collection.useApi;
+
+/**
+ * @author wuzhangchao
+ * @version 1.0
+ * @date 2023/11/6 17:26
+ */
+public class ArrayListApiTest {
+}
